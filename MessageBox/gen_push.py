3efd6a90aca0f91c8989f2807d9d73fa92a562bd
PhuocHT102 <miniphuoc@gmail.com>
2025-03-19 09:01:54 +0700

    Update gen_push.py

--- MessageBox/gen_push.py
+++ MessageBox/gen_push.py
@@ -2,12 +2,9 @@ n=input("Nhap ky tu can push: ")
 print(n)
 n=n[::-1]
 print("push 0")
-if len(n)%4:
-    c=(len(n)%4)
-    n="a"*(4-c)+n
-    print(f'push "{n[4-c:4]}"')
-else:
-    print(f'push "{n[:4]}"')
+c=(len(n)%4)
+n="a"*((4-c)%4)+n
+print(f'push "{n[(4-c)%4:4]}"')
 
 for i in range(4,len(n),4):
     print(f'push "{n[i:i+4]}"')
